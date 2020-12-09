55315a13b3ea60d1997f9397231d23fe61ea12cd
vtsatish <vtsatish@rediffmail.com>
2020-12-09 21:59:36 +0530

    restore format

--- src/app_intro.py
+++ src/app_intro.py
@@ -23,7 +23,7 @@ else:
 
 for num in range(1, 10, 3):
     print("Attempt", num, num * '.')
-    print("Val is {num} and attempt {num + 1} times")
+    print(f"Val is {num} and attempt {num + 1} times")
 
 for char in patient_name:
     print(char)
