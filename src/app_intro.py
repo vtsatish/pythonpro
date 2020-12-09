4244bf15ddaee2114770a936c73e78810ac4373b
vtsatish <vtsatish@rediffmail.com>
2020-12-09 21:22:58 +0530

    Learnbasics (#2)
    * move old samples
    
    * additional print
    
    * initial intro sessions
    
    initial intro sessions
    
    * intro complete
    
    intro complete
    
    * complete programs
    
    complete programs

---
+++ src/app_intro.py
@@ -0,0 +1,29 @@
+import math
+
+patient_name = 'John'
+patient_age = 30
+is_registered = True
+print(patient_age, patient_name)
+x = 1.9
+test = 3
+print(x + test)
+
+print(len(patient_name))
+print(patient_name.upper())
+
+print(math.ceil(x))
+
+if patient_age > 1:
+    print("Young guy")
+elif patient_age > 10:
+    print("Big guy")
+else:
+    print("Small")
+
+
+for num in range(1, 10, 3):
+    print("Attempt", num, num * '.')
+    print(f"Val is {num} and attempt {num + 1} times")
+
+for char in patient_name:
+    print(char)
---
+++ testDir/initialTest.py
@@ -0,0 +1,11 @@
+print('welcome to the world of: \n')
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
+
+print('Python')
+
+print('\n stay strong and learn')
+
+
