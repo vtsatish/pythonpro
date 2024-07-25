9b64cb4a8994be03e66bdb03386dfb756556bf77
vtsatish kumar <vtsatish@rediffmail.com>
2024-07-25 14:21:13 +0530

    api tests

---
+++ src/pyTest/test_apisuite.py
@@ -0,0 +1,33 @@
+import requests
+import wheel
+
+
+def test_api_get():
+    gourl = "https://reqres.in/api/users?page=2"
+    resp = requests.get(gourl)
+    assert resp.status_code == 200
+    resp_dict = resp.json()
+    print(resp_dict['page'])
+    data_list = resp_dict['data']
+    counter = 0
+    for record in data_list:
+        counter = counter + 1
+        print(counter, "record is \n")
+        for key in record:
+            print("record", counter, "property name is ", key)
+            print("record", counter, "property value is", record[key])
+
+    assert counter == 6
+
+
+def test_api_post():
+    gourl = "https://reqres.in/api/users"
+    request_data = {
+        'name': 'cool getter',
+        'job': 'all rounder'
+    }
+    resp = requests.post(gourl,data=request_data)
+    resp_data = resp.json()
+    print(resp_data)
+    assert resp.status_code == 201
+
